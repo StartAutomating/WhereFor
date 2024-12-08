5cdf8bb70524ac99737c5e96f6b55013668d9e4c
James Brundage <+@noreply.github.com>
2024-12-08 12:02:32 -0800

    test: Additional WhereFor Tests ( Fixes #10 )

--- WhereFor.tests.ps1
+++ WhereFor.tests.ps1
@@ -5,4 +5,10 @@ describe WhereFor {
         } | Should -Be "2 is even"
     }
 
+    it 'Will process a pipeline in order and run multiple actions' {
+        @(1..3 | ?% @{
+            { $_ % 2 } = { "$_ is odd" }
+            { -not ($_ % 2) } = { "$_ is even" }
+        }) -join "`n" | Should -Be "1 is odd`n2 is even`n3 is odd"
+    }
 }
